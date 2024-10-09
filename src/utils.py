f4c13580740f052370bcf3ff91499dffc4446ca1
NazKar@Nazir <karimov.nazir00@yandex.ru>
2024-10-09 16:30:17 +0500

    Add function and tests him. Add logging in utils.py Small fixes logging in utils.py Small fixes in utils.py file before logging

--- src/utils.py
+++ src/utils.py
@@ -3,17 +3,6 @@ import logging
 import os
 from typing import Dict, List
 
-# Создаем хэндлер
-handler = logging.FileHandler("logs.log", mode="w", encoding="utf-8")
-formatter = logging.Formatter(
-    "%(asctime)s:%(module)s:%(levelname)s:%(name)s:%(message)s"
-)
-handler.setFormatter(formatter)
-
-# Настраиваем логирование
-logging.basicConfig(level=logging.INFO, handlers=[handler])
-
-logger = logging.getLogger()
 
 path = "data/operations.json"
 
@@ -25,26 +14,24 @@ def load_transactions(path: str) -> List[Dict]:
     logger.info("Запуск операции...")
     # Проверяем на существование файла
     if not os.path.exists(path):
-        logger.error("Файл не найден.")
+        print("Файл не найден.")
         return []
 
-    logger.info("Начало загрузки данных")
 
     # Открываем файл по переданному пути
     with open(path, "r", encoding="utf-8") as file:
         try:
             # Загружаем данные JSON
             data_operations = json.load(file)
-            logger.info(
+            print(
                 "Данные успешно загружены: %s", str(data_operations)
             )  # Отладочный вывод
         except json.JSONDecodeError:
-            logger.error("Неподдерживаемый формат данных")
             return []
 
     # Проверяем, является ли загруженный объект списком
     if not isinstance(data_operations, list):
-        logger.error("Загруженные данные не являются списком.")
+        print("Загруженные данные не являются списком.")
         return []
 
     valid_transactions = []  # Список для хранения валидных транзакций
@@ -65,7 +52,6 @@ def load_transactions(path: str) -> List[Dict]:
                     transaction
                 )  # Добавляем валидную транзакцию в список
 
-    logger.info("Валидные транзакции загружены: %s", str(valid_transactions))
     return valid_transactions  # Возвращаем валидные транзакции
 
 
