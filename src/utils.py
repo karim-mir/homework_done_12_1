17ec2f37ad227834698be692c9da36b49cd74cf5
NazKar@Nazir <karimov.nazir00@yandex.ru>
2024-10-09 15:26:52 +0500

    Add function and tests him. Add logging in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,20 @@
 import json
+import logging
 import os
 from typing import Dict, List
+
+#Создаем хэндлер
+handler = logging.FileHandler('logs.log', mode='w', encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+handler.setFormatter(formatter)
+
+# Настраиваем логирование
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[handler]
+)
+
+logger = logging.getLogger()
 
 path = "data/operations.json"
 
@@ -9,23 +23,29 @@ def load_transactions(path: str) -> List[Dict]:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей
     с данными о финансовых транзакциях"""
 
+    logger.info("Запуск операции...")
     # Проверяем на существование файла
     if not os.path.exists(path):
-        print("Файл не найден.")
+        logger.error("Файл не найден.")
         return []
+
+    logger.info("Начало загрузки данных")
 
     # Открываем файл по переданному пути
     with open(path, "r", encoding="utf-8") as file:
         try:
             # Загружаем данные JSON
             data_operations = json.load(file)
-            print("Данные успешно загружены:", data_operations)  # Отладочный вывод
+            logger.info(
+                "Данные успешно загружены: %s", str(data_operations)
+            )  # Отладочный вывод
         except json.JSONDecodeError:
+            logger.error("Неподдерживаемый формат данных")
             return []
 
     # Проверяем, является ли загруженный объект списком
     if not isinstance(data_operations, list):
-        print("Загруженные данные не являются списком.")
+        logger.error("Загруженные данные не являются списком.")
         return []
 
     valid_transactions = []  # Список для хранения валидных транзакций
@@ -46,6 +66,7 @@ def load_transactions(path: str) -> List[Dict]:
                     transaction
                 )  # Добавляем валидную транзакцию в список
 
+    logger.info("Валидные транзакции загружены: %s", str(valid_transactions))
     return valid_transactions  # Возвращаем валидные транзакции
 
 
